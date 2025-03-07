47348685b63f20635db9f1dcf735f8b6a5c0be40
R1nex <kaspar.shanin@gmail.com>
2025-03-07 11:26:25 +0200

    mangija ja statistika esmane

--- Mangija.java
+++ Mangija.java
@@ -1,4 +1,34 @@
 public class Mangija {
-    //TEST
-    // Siia mängija loogika ja andmed
+    // Mangija info siia
+    private String nimi;
+    private Double kassa;
+
+    public Mangija(String nimi, Double kassa) {
+        this.nimi = nimi;
+        this.kassa = kassa;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public Double getKassa() {
+        return kassa;
+    }
+
+    public void setKassa(Double kassa) {
+        this.kassa = kassa;
+    }
+
+    @Override
+    public String toString() {
+        return "Mangija{" +
+                "nimi='" + nimi + '\'' +
+                ", kassa=" + kassa +
+                '}';
+    }
 }
