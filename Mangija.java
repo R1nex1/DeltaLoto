fb0c9977a419096458df6376aa78945cf87f48b8
AlekseiSamrai <aleksei.samrai@ut.ee>
2025-03-07 10:39:15 +0200

    Test3
    Muudatus3

--- Mangija.java
+++ Mangija.java
@@ -1,6 +1,7 @@
 public class Mangija {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello again");
     }
     // Siia mängija loogika ja andmed
 }
