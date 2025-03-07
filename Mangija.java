e3629d111cf2b747d91607f01bba4078db5091b7
AlekseiSamrai <aleksei.samrai@ut.ee>
2025-03-07 10:35:54 +0200

    TestCommit

--- Mangija.java
+++ Mangija.java
@@ -1,6 +1,6 @@
 public class Mangija {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hello World");
     }
     // Siia mängija loogika ja andmed
 }
